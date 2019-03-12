3a6d49e9faeb8d7d03780e4be36018585367a4ee
EMILIO10P <200375063@student.georgianc.on.ca>
2019-03-12 15:13:34 -0400

    Add project files.

---
+++ LMCStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LMCStore.MvcApplication" Language="C#" %>
---
+++ LMCStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LMCStore.Startup))]
+namespace LMCStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
